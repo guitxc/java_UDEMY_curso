51564b9de28e64a7e2651ca0157ead8670bf5e3b
guitxc <guisandolidna@gmail.com>
2025-05-09 15:13:38 -0300

    Exercicios 5 e 6

--- src/listaDeExer1.java
+++ src/listaDeExer1.java
@@ -54,6 +54,52 @@ public class listaDeExer1 {
 //        System.out.println("NUMBER = "+ funcionario);
 //        System.out.printf("SALARY = %.2f%n", salarioTotal);
 
+        // Fazer um programa para ler o código de uma peça 1, o número de peças 1, o valor unitário de cada peça 1, o
+        //código de uma peça 2, o número de peças 2 e o valor unitário de cada peça 2. Calcule e mostre o valor a ser pago
+//        System.out.println("Digite o codigo ,o numero de peças e valor unitario de duas peças:");
+//        int cod1, qtd1, cod2, qtd2;
+//        double valor1, valor2, total;
+//        cod1 = sc.nextInt();
+//        qtd1 = sc.nextInt();
+//        valor1 = sc.nextDouble();
+//
+//        cod2 = sc.nextInt();
+//        qtd2 = sc.nextInt();
+//        valor2 = sc.nextDouble();
+//
+//        total = (valor1 * qtd1) + (valor2 * qtd2);
+//
+//        System.out.printf("VALOR A PAGAR: R$ %.2f%n", total);
+
+
+
+        //Fazer um programa que leia três valores com ponto flutuante de dupla precisão: A, B e C. Em seguida, calcule e
+        //mostre:
+        //a) a área do triângulo retângulo que tem A por base e C por altura.
+        //b) a área do círculo de raio C. (pi = 3.14159)
+        //c) a área do trapézio que tem A e B por bases e C por altura.
+        //d) a área do quadrado que tem lado B.
+        //e) a área do retângulo que tem lados A e B.
+//        System.out.println("Digte 3 valores com ponto flutuante");
+//        double A,B,C, tri, cir, trap, qua, ret, pi = 3.14159;
+//        A = sc.nextDouble();
+//        B = sc.nextDouble();
+//        C = sc.nextDouble();
+//        tri = (A * C) / 2;
+//        System.out.printf("a) a área do triângulo retângulo que tem A por base e C por altura é : %.3f%n",  tri);
+//        cir = (pi * Math.pow(C, 2));
+//        System.out.printf("b) a área do círculo de raio C é : %.3f%n",  cir);
+//        trap = ((A + B) * C) / 2;
+//        System.out.printf("c) a área do trapézio que tem A e B por bases e C por altura é : %.3f%n",  trap);
+//        qua = Math.pow(B, 2);
+//        System.out.printf("d) a área do quadrado que tem lado B é : %.3f%n",  qua);
+//        ret = A * B;
+//        System.out.printf("e) a área do retângulo que tem lados A e B é : %.3f%n",  ret);
+
+
+
+
+
 
 
 
